65f9afe244847ffcf1d354e0d2e352fe203dca17
ZhekaProff <Zhekakad@ya.ru>
2022-06-09 18:36:07 +0900

    Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем

---
+++ Lesson03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int Any(int num1, int num2, int pos)
+{
+    if(pos <= 1) return num1 + num2;
+    return Any(num1, num2, pos - 1) + Any(num1, num2, pos - 2);
+}
+Console.WriteLine("Which first number?");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Which second number?");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("How much numbers?");
+int size = int.Parse(Console.ReadLine());
+for(int i = 1; i <= size; i++)
+{
+    Console.Write($"{Any(first,second,i)} ");
+}
