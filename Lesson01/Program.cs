06dcd7ebe8e3d7276d050153be95bbcdd78e3b51
ZhekaProff <Zhekakad@ya.ru>
2022-06-09 17:04:55 +0900

    первая задача на сумму чисел Рекурсия

---
+++ Lesson01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму цифр числа
+int RecSum(int num)
+{
+    if (num % 10 == 0)
+        return num;
+    return num % 10 + RecSum(num / 10);
+}
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма цифр числа = {Math.Abs(RecSum(num))}");
