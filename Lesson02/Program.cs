69abb2611ac4928fec0bbe98cf4ee1204ed77eb7
ZhekaProff <Zhekakad@ya.ru>
2022-06-09 18:34:36 +0900

    Написать программу возведения числа А в целую стень B

---
+++ Lesson02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int PowAB(int a, int b)
+{
+    if(b==0) return 1;
+    else return PowAB(a, b - 1) * a;
+}
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()?? "");
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine($"Число {a} в степени {b} равно {PowAB(a,b)}");
